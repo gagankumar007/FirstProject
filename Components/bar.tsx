ff6afdd49eef3a4ac9ef1a7a0fddb1597738e0f2
gagan <gk909999@gmail.com>
2024-03-28 13:23:20 +0530

    ios bulid

--- Components/bar.tsx
+++ Components/bar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import{View,Text,SafeAreaView,Button} from "react-native"
+import{View,Text,SafeAreaView,Button, StyleSheet} from "react-native"
 
 
 export function Bar(){
@@ -7,9 +7,28 @@ export function Bar(){
   <SafeAreaView>
     <View>
       <Text>hello react from gagan</Text>
+      <View style={Styles.contnaier}>
+      <Text style={Styles.whiteText}>hello</Text>
+      </View>
     </View>
     <Button title="gagan">
        </Button>
   </SafeAreaView>
   )
 }
+
+const Styles = StyleSheet.create({
+  contnaier:{
+    width:100,
+    height:100,
+    backgroundColor:"#000000",
+    
+
+  },
+  whiteText:
+  {
+    color:'#ffffff',
+    flex:1,
+    justifyContent:"center"
+  }
+})
