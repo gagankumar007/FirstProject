06195e29f4ca5b525b03dfa96abbdf52a410fd02
gagan <gk909999@gmail.com>
2024-03-28 12:32:12 +0530

    edit AppPro.tsx

--- AppPro.tsx
+++ AppPro.tsx
@@ -1,16 +1,26 @@
 import React from "react";
-import{View,Text,SafeAreaView,Button} from "react-native"
+import{View,Text,SafeAreaView,Button, StyleSheet} from "react-native"
+import { Bar } from "./Components/bar";
 
 
 function AppPro(){
   return(
   <SafeAreaView>
     <View>
-      <Text>hello react from gagan</Text>
+      <Text style={Styles.contnaier}>hello react from gagan</Text>
     </View>
     <Button title="gagan">
        </Button>
+       <Bar/>
   </SafeAreaView>
   )
 }
+
+const Styles=StyleSheet.create(
+  {
+    contnaier:{
+      fontSize:34
+    }
+  }
+)
 export default AppPro;
